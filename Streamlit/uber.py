ca3469af2bff77a75b803d2d65369d0f9aae096c
Ирина Скрипка <32329115+IrinaSkripka99@users.noreply.github.com>
2020-03-01 22:03:14 +0300

    Create uber.py

---
+++ Streamlit/uber.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+"""
+# Uber Pickups in New York City
+"""
+"""
+"""
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+
+data = load_data(100000)
+'data',data
+
+hour=st.sidebar.slider('hour',0,23,10)
+# hour=st.sidebar.number_input('hour',0,23,10)
+data=data[data[DATE_TIME].dt.hour==hour]
+
+'## Geo Data at %sh' % hour
+st.map(data)
+''
+midpoint = (np.average(data["lat"]), np.average(data["lon"]))
+
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50,
+    },
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data,
+            get_position=["lon", "lat"],
+            radius=100,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+    ],
+))
+if st.checkbox('Show Raw Data'):
+    '## Raw Data at %sh' % hour,data
